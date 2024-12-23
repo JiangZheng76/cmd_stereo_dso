0083672f51a91bf4a9fc7cd1412daab1fc415adb
jiangzheng <2440877322@example.com>
2024-12-23 23:02:04 +0800

    cmd_stereo_dso:通讯正常,回环检测有误

--- bash/cmd_kitti00.bash
+++ bash/cmd_kitti00.bash
@@ -1,6 +1,8 @@
-bin_path="~/cmd_ws/devel/lib/stereo_dso/"
+bin_path="/home/sysu/cmd_ws/devel/lib/stereo_dso"
 sequences=/media/sysu/200GB_Other/Dataset/Slam/KITTI/sequences/00/multi-robot
 calib=/home/sysu/cmd_ws/src/cmd-slam/vo/stereo-dso/cams/kitti/0_2/camera.txt
+
+cd ${bin_path}
 
 ./dso_dataset files=${sequences}/1/ calib=${calib} preset=0 mode=1 quiet=1 nomt=1 &
 
